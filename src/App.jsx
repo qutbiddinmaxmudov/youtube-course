c0df3a2e828b93ab79d7abd42625b6fd92a7f644
Maxmudov <qutbiddinmaxmudov@mail.ru>
2022-05-15 20:07:43 +0500

    made initial layout

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Home from "./pages/Home";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from './Home.module.scss'
+
+const Home = () => {
+  return (
+    <div className={classes['root']}>Home</div>
+  )
+}
+
+export default Home
